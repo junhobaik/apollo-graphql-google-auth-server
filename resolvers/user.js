1604902aa4fd97d27d5087a207c718ad810efa0c
Junho Baik <junhobaik@gmail.com>
2019-09-18 21:43:13 +0900

    user reslover, users query 수정

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,7 +1,7 @@
 export default {
   Query: {
-    users: async (parent, args, {}) => {
-      return [{ _id: 'TEST_ID', name: 'TEST_NAME' }];
+    users: async (parent, args, { userModel }) => {
+      return await userModel.find({}, (err, users) => users);
     },
 
     user: async (parent, args, {}) => {
