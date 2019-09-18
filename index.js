ecd64acaae9fb7041f2fb1566206f3867fc1df1d
Junho Baik <junhobaik@gmail.com>
2019-09-18 21:06:35 +0900

    ApolloServer 수정 및 cors 설정, session 등록

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
-import { ApolloServer } from 'apollo-server';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import cors from 'cors';
+
 import typeDefs from './schema';
 import resolvers from './resolvers';
+import session from './lib/session';
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const app = express();
+require('dotenv').config();
 
-server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
+let serverUrl;
+let localeUrl;
+if (process.env.NODE_ENV === 'development') {
+  serverUrl = 'http://localhost:4000';
+  localeUrl = 'http://localhost:3000';
+} else {
+  serverUrl = process.env.AWS_PUBLIC_DNS;
+  localeUrl = 'http://localhost:3000'; // 차후 수정
+}
+console.log('> serverUrl: ', serverUrl);
+console.log('> localeUrl: ', localeUrl);
+
+const corsOptions = {
+  origin: [localeUrl, serverUrl],
+  credentials: true
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => {
+    if (req.user) console.log(req.user);
+    return null;
+  }
 });
+
+app.use(session);
+app.use(cors(corsOptions));
+server.applyMiddleware({ app, cors: false });
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at ${server.graphqlPath}`)
+);
